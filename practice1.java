f0f3169117ad2443132b4c32a32b21538cde78f2
eileenguoo <42676374+eileenguoo@users.noreply.github.com>
2018-12-04 21:46:38 -0500

    Update practice1.java

--- practice1.java
+++ practice1.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 /*
 ask user for student ID
-check if Id exists in the library system
+check if Id exists in the libarary system
 if yes, add current borrowing book to the the Id account
 if not, create a new id account for the student, with name and book
 system automatically generates the checkout and return date
@@ -14,38 +14,40 @@ the system uses 30 day book return period
  */
 
 
-public class practice1 {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+public class Library {
+
+    private static PrintStream outputFileStream;
+
+    public static void main(String[] args) throws Exception {
         Scanner console = new Scanner(System.in);
-        PrintStream output = new PrintStream(outputFile(console));
+        outputFileStream = new PrintStream(outputFile(console));
         ArrayList<String> name = new ArrayList<>();
         ArrayList<Integer> id = new ArrayList<>();
-        ArrayList<ArrayList<String>> outer = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> outer = new ArrayList<>();
         ArrayList<String> inner = new ArrayList<>();
-        Scanner f = new Scanner(new File("datsbase.txt"));
+        Scanner f = new Scanner(new File("/Users/Eileen/IdeaProjects/Practice/WorkingFiles/aaa.txt"));
+
 
         ArrayList<String> n = new ArrayList<>();
         ArrayList<String> r = new ArrayList<>();
         String a = "y";
-        String line = "";
-        int j = 0;
-        int z = 0;
-        int ct = 0;
+        int j;
 
         ArrayList<String> all = new ArrayList<>();
         while (f.hasNextLine()) {
             all.add(f.nextLine());
         }
         System.out.println(all);
-        System.out.println(all.get(5));
 
         while (a.contains("y")) {
             System.out.print("Student ID: ");
             int t = console.nextInt();
+            console.nextLine();
             if (id.contains(t)) {
                 j = id.indexOf(t);
                 System.out.print("Book: ");
-                String value = console.next();
+                String value = console.nextLine();
+                System.out.println("\n\n$$$ " + value + " $$$\n\n");
                 boolean ftt = bookExist(f, value, all);
                 if (ftt) {
                     outer.get(j).add(value);
@@ -56,9 +58,10 @@ public class practice1 {
             } else {
                 id.add(t);
                 System.out.print("Student name: ");
-                name.add(console.next());
+                name.add(console.nextLine());
                 System.out.print("Checkout Book: ");
-                String vv = console.next();
+                String vv = console.nextLine();
+                System.out.println("\n\n### " + vv + " ###\n\n");
                 boolean tff = bookExist(f, vv, all);
                 if (tff) {
                     ArrayList<String> nw = new ArrayList<>();
@@ -80,27 +83,27 @@ public class practice1 {
             System.out.print("Continue(y/n): ");
             a = console.next();
         }
-        System.setOut(output);
-        outFile(console, name, id, outer, inner, r, n);
+
+        outFile(name, id, outer, r, n);
     }
 
     public static File outputFile(Scanner console) {
-        System.out.print("User Data File Name: ");
-        File output = new File(console.next());
+        System.out.print("Output file name: ");
+        File output = new File(console.nextLine());
         return output;
     }
 
     public static boolean bookExist(Scanner f, String value, ArrayList<String> all) {
         boolean tf = false;
         for(String ss : all){
-            if (ss.contains(value))
+            if (ss.equals(value))
                 tf = true;
         }
         return tf;
     }
 
-    public static void outFile(Scanner console, ArrayList<String> name, ArrayList<Integer> id,
-                               ArrayList<ArrayList<String>> outer, ArrayList<String> inner,
+    public static void outFile(ArrayList<String> name, ArrayList<Integer> id,
+                               ArrayList<ArrayList<String>> outer,
                                ArrayList<String> r, ArrayList<String> n ) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < id.size(); i++) {
@@ -111,7 +114,6 @@ public class practice1 {
             sb.append("Return Date: ").append(r.get(i)).append("\n");
             sb.append("Book List: ").append(outer.get(i)).append("\n\n");
         }
-        System.out.print(sb);
-
+        outputFileStream.print(sb);
     }
 }
